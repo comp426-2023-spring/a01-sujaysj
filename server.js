15436d7093b563e72654e017cbff8052b809d42a
sujaysj <sujaysj@gmail.com>
2023-04-27 08:14:47 -0400

    bug fixing

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ const port = args.port || 3000;
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-const readIndex = () => {
+const readIndex = (callback) => {
     fs.readFile('./public/index.html', 'utf8', (err, data) => {
         if (err) {
             console.error(err)
             return
         }
-        return data
+        callback(data);
     })
 }
 
@@ -43,7 +43,9 @@ const readIndex = () => {
 const server = http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/html')
-    res.end(readIndex())
+    readIndex((data) => {
+        res.end(data);
+    })
 })
 
 
